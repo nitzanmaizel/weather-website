19381c58d903663d3f0193bf68dc6b157a04233a
Nitzan Maizel <nitzanmaizel@gmail.com>
2020-12-19 18:55:51 +0200

    change for the output

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,16 +2,22 @@ const request = require("request");
 
 const forecast = (latitude, longitude, callback) => {
 	const url = `http://api.weatherstack.com/current?access_key=5d2ff90952d3350c52994d1a55643ef4&query=${latitude},${longitude}`;
+	console.log(url);
 	request({ url, json: true }, (error, response) => {
 		if (error) {
 			callback("Unable to connect to weather service!");
 		} else if (response.body.error) {
 			callback(`Unable to find location`);
 		} else {
+			console.log(response);
 			const { temperature, feelslike, weather_descriptions } = response.body.current;
+			const { localtime } = response.body.location;
 			callback(
 				undefined,
-				`${weather_descriptions}. It is currently ${temperature}${"\u00B0"} out. It feels like ${feelslike}${"\u00B0"} out. `
+				`${weather_descriptions}.
+				 It is currently ${temperature}${"\u00B0"} out.
+				 It feels like ${feelslike}${"\u00B0"} out.
+				 ${localtime}   `
 			);
 		}
 	});
